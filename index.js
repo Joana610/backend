8033a93e06189a880f14a79836cea71e74939d57
pandzicjoana <pandzicjoana@gmail.com>
2026-01-30 00:20:41 +0100

    update  index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeRoutes from "./routes/home.js";
+import kategorijeRoutes from "./routes/kategorije.js";
 import komentariRoutes from "./routes/komentari.js";
 import korisniciRoutes from "./routes/korisnici.js";
 import mjestaRoutes from "./routes/mjesta.js";
@@ -8,11 +9,13 @@ import posjecenoRoutes from "./routes/posjeceno.js";
 const app = express();
 app.use(express.json());
 
-app.use("/", homeRoutes);
-app.use("/", posjecenoRoutes);
-app.use("/", mjestaRoutes);
-app.use("/", komentariRoutes);
+app.use("/", homeRoutes);       
+app.use("/mjesta", mjestaRoutes);
+app.use("/kategorije", kategorijeRoutes);
+app.use("/komentari", komentariRoutes);
+app.use("/posjeceno", posjecenoRoutes);
 app.use("/korisnici", korisniciRoutes);
+
 
 app.get("/test", (req, res) => {
   res.json({ ok: true });
